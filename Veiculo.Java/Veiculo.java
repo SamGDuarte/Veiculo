f679275ee13a5736aaca8216b5a6eda7b3f9ffdf
SamGDuarte <126623537+SamGDuarte@users.noreply.github.com>
2023-03-14 20:03:36 -0300

    Add files via upload

---
+++ Veiculo.Java/Veiculo.java
@@ -0,0 +1,8 @@
+public class Veiculo{
+public static void main(String[] args) {
+    int Peso;
+    int Kmh;
+    float Preco;
+    String Modelo;
+}
+}
---
+++ Veiculo.Java/caminhao.java
@@ -0,0 +1,32 @@
+public class caminhao {
+    private int Peso;
+    private int Kmh;
+    private float Preco;
+    private String Modelo;
+    
+    public void setPeso(int peso) {
+        Peso = peso;
+    }
+    public void setKmh(int kmh) {
+        Kmh = kmh;
+    }
+    public void setPreco(float preco) {
+        Preco = preco;
+    }
+    public void setModelo(String modelo) {
+        Modelo = modelo;
+    }
+    public int getPeso() {
+        return Peso;
+    }
+    public int getKmh() {
+        return Kmh;
+    }
+    public float getPreco() {
+        return Preco;
+    }
+    public String getModelo() {
+        return Modelo;
+    }
+
+}
